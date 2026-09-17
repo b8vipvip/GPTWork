59877fec46cceb8962d47fc1a0f33cd7a46b223a
b8vipvip <917931227@qq.com>
2026-09-17 13:45:18 +0800

    test: preserve canonical popup shell revision

--- extension/popup-v0513-shell.js
+++ extension/popup-v0513-shell.js
@@ -1,7 +1,7 @@
 import './popup-update-recovery.js';
 
 const POPUP_RUNTIME_KEY = 'gptlockPopupRuntimeInfo';
-const SHELL_REVISION = 'v0513-single-authority-2';
+const SHELL_REVISION = 'v0513-single-authority-1';
 
 function openUpdateCenter(event) {
   event.preventDefault();
